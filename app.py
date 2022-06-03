39cabe6392dd37a1e085b432967ecbff57f94dd2
Severenkova <sae@iMac-mac.Dlink>
2022-06-03 22:08:34 +0300

    New templates

--- app.py
+++ app.py
@@ -1,27 +1,23 @@
 from flask import Flask, render_template
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./templates')
 
 @app.route('/')
 def main_page():
     title = "Добро пожаловать!"
     text = "Я рисую для души и хочу поделиться своим творчеством с вами! Вот несколько моих работ:"
-    description = "Картина номер 1"
-    return render_template('main_page.html', context={'title':'Добро пожаловать!', 'text':'Я рисую для души и хочу поделиться своим творчеством с вами! Вот несколько моих работ:', 'description':'Картина номер 1'})
+    footer = "Сайт Евгения Северенкова"
+    return render_template('main_page.html', context={'title':title, 'text':text, 'footer':footer})
 
 @app.route('/author')
 def about_author():
     return render_template('about_author.html')
 
-@app.route('/shop')
-def shop():
-    return render_template('shop.html')
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
 
-@app.route('/cart')
-def cart():
-    return render_template('cart.html')
-
-@app.route('/account')
-def account():
-    return render_template('account.html')
+@app.route('/edit_site')
+def edit_site():
+    return render_template('edit_site.html')
